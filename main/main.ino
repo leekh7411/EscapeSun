8b4e162459040a8c0e4a6790d1d6ad5a7c114ec1
2eeKwangHo <leekh7411@naver.com>
2017-09-20 13:32:39 +0900

    충돌 수정

--- main/main.ino
+++ main/main.ino
@@ -54,25 +54,18 @@ int tempDelay = 0;
 int tempHumidDelay = 0;
 void loop()
 {  
-
-  if (!stepdetection.stepEventsEnabeled) {
-     
-    stepdetection.updateStepCount();
-  }
-  //  currentTime.resetTime();  
+    if (!stepdetection.stepEventsEnabeled) {
+      stepdetection.updateStepCount();
+    }
+    //currentTime.resetTime();  
     //stepdetection.movement();
     Serial.print("초 : ");
-     Serial.println(currentTime.Secondtime());
-     Serial.print("분 : ");
-      Serial.println(currentTime.Minutetime());
- // blemanager.initInLoop(central,sensorData,distance,switch0,emergency,limit_distance,limit_heart_rate,limit_humidity);
-//  Serial.println(blemanager.getLimitDistance());
-  /*
-=======
-  blemanager.initInLoop(central,sensorData,distance,switch0,emergency,limit_distance,limit_heart_rate,limit_humidity);
-  Serial.println(blemanager.getLimitDistance());
-  
->>>>>>> 4cb89aa9eb7cefe9ec539e561632e0887bd897ee
+    Serial.println(currentTime.Secondtime());
+    Serial.print("분 : ");
+    Serial.println(currentTime.Minutetime());
+    blemanager.initInLoop(central,sensorData,distance,switch0,emergency,limit_distance,limit_heart_rate,limit_humidity);
+
+ 
   //Serial.println("in loop");
   long currentMillis = millis();
   // if 200ms have passed, check the heart rate measurement:
